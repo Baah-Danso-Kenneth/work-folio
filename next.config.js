b2fdca67b4b5942cf4f6b6e7984247089d6022e0
Kenneth Danso <kwadwobaahdanso@gmail.com>
2024-01-11 02:01:22 +0000

    bugs

--- next.config.js
+++ next.config.js
@@ -1,9 +1,11 @@
 /** @type {import('next').NextConfig} */
 
 const nextConfig = {
+  output: 'export',
   images: {
-    domains: ['work-folio-kenny.s3.eu-north-1.amazonaws.com',],
+    domains: ['work-folio-kenny.s3.eu-north-1.amazonaws.com'],
   },
 };
 
 module.exports = nextConfig;
+
